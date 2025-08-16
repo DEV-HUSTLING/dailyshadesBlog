929743c6f838ea17245301ebbdad36bb837b60f3
DEV-HUSTLING <anushkadevg@gmail.com>
2025-08-16 15:27:39 -0400

    mvp site built

---
+++ src/app/about/page.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import Image from "next/image";
+import Link from 'next/link'
+
+function page() {
+  return (
+    <div className="mainSection">
+      <header className="fixed w-full  bg-gray-300 flex justify-between p-4 pl-8 pr-8">
+        <Link href="/ " className="text-3xl">Dailyshades</Link>
+        <div className="text-xl flex gap-20">
+          <Link href="/work" className="underline">Work</Link>
+          <Link href="/about" className="underline">About Me</Link>
+        </div>
+      </header>
+      <main className="flex gap-8 absolute mt-40 sm:right-0 sm:top-20 sm:bottom-0 sm:left-0 sm:m-auto sm:w-full sm:flex-col-reverse sm:p-6 md:absolute md:right-0 md:bottom-auto md:left-auto md:top-1/2 md:-translate-y-1/2 md:w-1/2 md:flex-col-reverse">
+        <a href='https://www.instagram.com/dailyshades_explore?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==' target='blank'>
+        <p><u>Follow Me!</u></p><br/>
+        <Image
+            aria-hidden
+            src="/insta.png"
+            alt=" icon"
+            width={100}
+            height={500}
+             className="2xl:w-lg"
+          />
+          
+          </a>
+          <div  className="text-3xl 2xl:text-3xl font-light">
+        <p>
+          A Little about me, <br />
+            <br />
+        </p>
+        <ul className='font-300 text-wrap list-disc'>
+          <li>Hyderabadi girl.</li>
+            <li>Humble Family,</li>
+            <li className=''>Passion to build and create  anything from art to tech.</li>
+            <li>Love the process, win or lose</li>
+            </ul>
+            </div>
+      </main>
+      <footer className="-z-10 absolute bottom-0 left-50 ">
+          <Image
+            aria-hidden
+            src="/HydIcon.png"
+            alt=" icon"
+            width={400}
+            height={500}
+             className="2xl:w-lg"
+          />
+      </footer>
+    </div>
+  )
+}
+
+export default page
---
+++ src/app/work/page.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import Link from 'next/link'
+
+function page() {
+  return (
+    <div className='work'>
+      <header className="fixed z-10 w-full drop-shadow-sm  bg-gray-300 flex justify-between p-4 pl-8 pr-8 ">
+        <Link href="/ " className="text-3xl">Dailyshades</Link>
+        <div className="text-xl flex gap-20">
+          <Link href="/work" className="underline">Work</Link>
+          <Link href="/about" className="underline">About Me</Link>
+        </div>
+      </header>
+      <div className='absolute m-auto top-32 left-0 right-0 grid-main md:grid md:grid-cols-3 md:place-content-center md:gap-4 md:justify-items-center flex-col flex items-center gap-8 2xl:grid-cols-4'>
+        <div className='gallery-card bg-gray-200 rounded-md drop-shadow-sm flex flex-col gap-8 w-md h-48 p-4'>
+          <h3 className='text-4xl'><a href='https://anushkabio.netlify.app/' target='blank'>Perspective</a></h3>
+          <p>A small try to find calmness during chaos. </p><br/>
+          <span className='absolute bottom-0 flex w-full pr-8 justify-between align-center'>
+            <p className='text-xs'><b>1/2020-2/2021</b></p>
+            <p>blog</p>
+          </span>
+        </div>
+        <div className='gallery-card bg-gray-200 rounded-md drop-shadow-sm flex flex-col gap-8 h-48 w-md p-4'>
+          <h3 className='text-4xl'> <a href='' target='blank'>New York</a></h3>
+          <p>New phase, New angle but same old me</p><br/>
+          <span className='absolute bottom-0 flex w-full pr-8  justify-between align-center'>
+            <p className='text-xs'><b>1/2023-present</b></p>
+            <p>blog</p>
+          </span>
+        </div>
+        <div className='gallery-card bg-gray-200 rounded-md drop-shadow-sm flex flex-col gap-8 h-48 w-md p-4'>
+          <h3 className='text-4xl'>Yummy !!</h3>
+            <p>Amatuer cook but professional jugaadu </p><br/>
+          <span className='absolute bottom-0 flex w-full pr-8  justify-between align-center'>
+            <p className='text-xs'><b>1/2020-present</b></p>
+            <p>blog</p>
+          </span>
+        </div>
+
+         <div className='gallery-card bg-gray-200 rounded-md drop-shadow-sm overflow-hidden  text-wrap flex flex-col gap-8 h-48 w-md p-4 overflow-auto'>
+          <h3 className='text-4xl'>Resume</h3>
+            <p>Simple Resume Enhancer with the support of LLM </p><br/>
+          <span className='absolute bottom-0 flex w-full pr-10 justify-between align-center'>
+            <p className='text-xs'><b>6/2025-1/7/2025</b></p>
+            <p >blog</p>
+          </span>
+        </div>
+
+
+      </div>
+      
+    </div>
+  )
+}
+
+export default page
