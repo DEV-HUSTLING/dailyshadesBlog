493fe14884ddb77286b47b9b498a6d352dfa7ff6
DEV-HUSTLING <anushkadevg@gmail.com>
2025-09-21 15:38:19 -0400

    New cover page and added new card for work

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,18 @@
+'use client'
 import Image from "next/image";
 import Link from 'next/link'
 import Article from './article'
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from "react";
 export default function Home() {
+  useEffect(()=>{
+    AOS.init();
+
+  })
   return (
       <div className="mainSection">
-      <header className="fixed w-full  bg-gray-300 flex justify-between p-4 pl-8 pr-8">
+      <header className="fixed w-full border-b-2 border-gray-300 flex justify-between p-4 pl-8 pr-8 z-1">
         <Link href="/ " className="text-3xl">Dailyshades</Link>
         <div className="text-xl flex gap-20">
           <Link href="/work" className="underline">Work</Link>
@@ -13,20 +21,89 @@ export default function Home() {
       </header>
       <main className="absolute mt-20 sm:right-0 sm:top-20 sm:bottom-0 sm:left-0 sm:m-auto sm:w-full sm:p-6 md:absolute md:right-0 md:bottom-auto md:left-auto md:top-1/2 md:-translate-y-1/2 md:w-2/5">
         <p className="text-4xl 2xl:text-5xl ">
-          This is my place of creativity — a space not for impressing, but for expressing. 
+          This is my place of creativity. 
           Here, I share how I see the world I live in. <br />
           I believe beauty is so overrated that we often overlook the <span className="bg-gray-600 text-white p-1 text-3xl 2xl:text-4xl">perfectly imperfect</span> moments of everyday life.
         </p>
       </main>
       <footer className="absolute bottom-0 left-0 ">
+          <div className="flex w-full items-end justify-center">
+            <Image
+            aria-hidden
+            src="/home/h1.png"
+            alt=" icon"
+            width={130}
+            height={500}
+             className=""
+             data-aos="fade-up"
+          />
+          {/* <Image
+            aria-hidden
+            src="/home/h2.png"
+            alt=" icon"
+            width={130}
+            height={200}
+             className=""
+              data-aos="fade-up"
+             data-aos-delay="50"
+            data-aos-duration="1000"
+          /> */}
           <Image
             aria-hidden
-            src="/nyOutline.png"
+            src="/home/h3.png"
             alt=" icon"
-            width={800}
+            width={130}
             height={500}
-             className="2xl:w-6xl"
+             className=""
+               data-aos="fade-up"
+             data-aos-delay="150"
+            data-aos-duration="1000"
           />
+          <Image
+            aria-hidden
+            src="/home/h4.png"
+            alt=" icon"
+            width={130}
+            height={500}
+             className=""
+               data-aos="fade-up"
+             data-aos-delay="200"
+            data-aos-duration="1000"
+          />
+          <Image
+            aria-hidden
+            src="/home/h5.png"
+            alt=" icon"
+            width={130}
+            height={500}
+             className=""
+               data-aos="fade-up"
+             data-aos-delay="250"
+            data-aos-duration="1000"
+          />
+          <Image
+            aria-hidden
+            src="/home/h6.png"
+            alt=" icon"
+            width={130}
+            height={500}
+             className=""
+               data-aos="fade-up"
+             data-aos-delay="300"
+            data-aos-duration="1000"
+          />
+          <Image
+            aria-hidden
+            src="/home/h7.png"
+            alt=" icon"
+            width={130}
+            height={500}
+             className=""
+               data-aos="fade-up"
+             data-aos-delay="350"
+            data-aos-duration="1000"
+          />
+          </div>
       </footer>
     </div>
 
--- src/app/work/page.js
+++ src/app/work/page.js
@@ -4,7 +4,7 @@ import Link from 'next/link'
 function page() {
   return (
     <div className='work'>
-      <header className="fixed z-10 w-full drop-shadow-sm  bg-gray-300 flex justify-between p-4 pl-8 pr-8 ">
+      <header className="fixed w-full border-b-2 border-gray-300 flex justify-between p-4 pl-8 pr-8 z-1">
         <Link href="/ " className="text-3xl">Dailyshades</Link>
         <div className="text-xl flex gap-20">
           <Link href="/work" className="underline">Work</Link>
@@ -12,7 +12,7 @@ function page() {
         </div>
       </header>
       <div className='absolute m-auto top-32 left-0 right-0 grid-main md:grid md:grid-cols-3 md:place-content-center md:gap-4 md:justify-items-center flex-col flex items-center gap-8 2xl:grid-cols-4'>
-        <div className='gallery-card bg-gray-200 rounded-md drop-shadow-sm flex flex-col gap-8 w-md h-48 p-4'>
+        <div data-aos="zoom-in" className='gallery-card bg-gray-200 rounded-md drop-shadow-sm flex flex-col gap-8 w-md h-48 p-4'>
           <h3 className='text-4xl'><a href='https://anushkabio.netlify.app/' target='blank'>Perspective</a></h3>
           <p>A small try to find calmness during chaos. </p><br/>
           <span className='absolute bottom-0 flex w-full pr-8 justify-between align-center'>
@@ -20,7 +20,7 @@ function page() {
             <p>blog</p>
           </span>
         </div>
-        <div className='gallery-card bg-gray-200 rounded-md drop-shadow-sm flex flex-col gap-8 h-48 w-md p-4'>
+        <div data-aos="zoom-in" className='gallery-card bg-gray-200 rounded-md drop-shadow-sm flex flex-col gap-8 h-48 w-md p-4'>
           <h3 className='text-4xl'> <a href='' target='blank'>New York</a></h3>
           <p>New phase, New angle but same old me</p><br/>
           <span className='absolute bottom-0 flex w-full pr-8  justify-between align-center'>
@@ -28,7 +28,7 @@ function page() {
             <p>blog</p>
           </span>
         </div>
-        <div className='gallery-card bg-gray-200 rounded-md drop-shadow-sm flex flex-col gap-8 h-48 w-md p-4'>
+        <div data-aos="zoom-in" className='gallery-card bg-gray-200 rounded-md drop-shadow-sm flex flex-col gap-8 h-48 w-md p-4'>
           <h3 className='text-4xl'>Yummy !!</h3>
             <p>Amatuer cook but professional jugaadu </p><br/>
           <span className='absolute bottom-0 flex w-full pr-8  justify-between align-center'>
@@ -37,7 +37,7 @@ function page() {
           </span>
         </div>
 
-         <div className='gallery-card bg-gray-200 rounded-md drop-shadow-sm overflow-hidden  text-wrap flex flex-col gap-8 h-48 w-md p-4 overflow-auto'>
+         <div data-aos="zoom-in" className='gallery-card bg-gray-200 rounded-md drop-shadow-sm overflow-hidden  text-wrap flex flex-col gap-8 h-48 w-md p-4 overflow-auto'>
           <h3 className='text-4xl'>Resume</h3>
             <p><a href="https://resumifyy.netlify.app/" target='blank'>Simple Resume Enhancer with the support of LLM </a></p><br/>
           <span className='absolute bottom-0 flex w-full pr-10 justify-between align-center'>
@@ -46,6 +46,14 @@ function page() {
           </span>
         </div>
 
+        <div data-aos="zoom-in" className='gallery-card bg-gray-200 rounded-md drop-shadow-sm flex flex-col gap-8 w-md h-48 p-4'>
+          <h3 className='text-4xl'><a href='https://vaishnavikrishna.netlify.app/' target='blank'>Minimalist Portfolio</a></h3>
+          <p>An expression of a mother. </p><br/>
+          <span className='absolute bottom-0 flex w-full pr-8 justify-between align-center'>
+            <p className='text-xs'><b>9/2025-9/2025</b></p>
+            <p>nextJs</p>
+          </span>
+        </div>
 
       </div>
       
